87cdc8731d701cda1c2e17690545dd7d90e7694c
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-03-10 18:04:31 +0300

    Ejs Template|Projede ejs şablonu kullanmak için gerekli işlemler yapıldı. Ejs template i projemizde fotoğraf eklemek, açıklaam eklemek gibi dinamik işlemleri response olarak dönmek üzere kullandık.

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const express = require('express');
-
-const path = require('path');
+const express = require('express'); //express i app.js sayfasına dahil etme
+const path = require('path'); //statik dosyaları response olarak döndürmek için path modülünü app.js sayfasına dahil etme
+const ejs = require('ejs'); //ejs tempalte şablonunu kullanmak için ejs modülünü app.js sayfasına dahil etme
 
 const app = express();
 
-//MIDDLEWARES
-app.use(express.static('public'));  //index.html,css gibi statik dosyaları ekleme
+/*EJS modülü template dosyaları görebilmek için varsayılan olarak views klasörünün içerisindeki .ejs uzantılı dosyalara bakar. Bu ne denle temp dosyamızın ismini views olarak değiştiriyoruz. Videws klasörü içerisindeki tüm .html uzantılarını .ejs olarak değiştiriyoruz.  */
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs'); //ejs yi kullanacağımızı gösteriyoruz.
+
+//MIDDLEWARES
+app.use(express.static('public')); //index.html,css gibi statik dosyaları ekleme
+
+//ROUTES
 app.get('/', (req, res) => {
-  //template klasöründeki index.html dosyasını resolve olarak döndürme
-  res.sendFile(path.resolve(__dirname, 'template/index.html'));
+  //Uygulamamızdaki .get metodunu düzenlersek, bu şekilde '/' isteğine karşılık index.ejs dosyasını render ederiz.
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  //Uygulamamızdaki .get metodunu düzenlersek, bu şekilde '/' isteğine karşılık index.ejs dosyasını render ederiz.
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  //Uygulamamızdaki .get metodunu düzenlersek, bu şekilde '/' isteğine karşılık index.ejs dosyasını render ederiz.
+  res.render('add');
 });
 
 const port = 3000;
